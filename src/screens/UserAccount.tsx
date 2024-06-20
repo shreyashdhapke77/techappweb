89bbc1b190c25d6832e8ff7f97d2c8b1013d35eb
Pravin Salame <pravin.salame@utilizecore.com>
2024-06-20 12:16:49 +0530

    - Updated icons and some code

---
+++ src/screens/UserAccount.tsx
@@ -0,0 +1,12 @@
+import ApplicationBar from "../components/ApplicationBar"
+
+const UserAccount = () => {
+    return (
+        <div>
+            <ApplicationBar/>
+            <h1>This is User Account Page</h1>
+        </div>
+    )
+}
+
+export default UserAccount
