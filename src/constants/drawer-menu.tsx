0bfd58eeb9bdd992081413192b4c9693f612346c
Shreyash Dhapke <shreyash@utilizecore.com>
2024-07-06 14:15:22 +0530

    Cleaned Code of Application Bar

---
+++ src/constants/drawer-menu.tsx
@@ -0,0 +1,40 @@
+import { Feed, HandshakeTwoTone, HomeRepairService, PermPhoneMsg, StarPurple500 } from "@mui/icons-material";
+
+export const drawerMenu = {
+  menuOne: [
+    {
+      text: 'Find a worker',
+      icon: HandshakeTwoTone,
+      path: '/findWorker'
+    },
+    {
+      text: 'Become a worker',
+      icon: HomeRepairService,
+      path: '/becomeWorker'
+    }
+  ],
+  menuTwo: [
+    {
+      text: 'About Us',
+      icon: Feed,
+      path: '/about'
+    },
+    {
+      text: 'Contact Us',
+      icon: PermPhoneMsg,
+      path: '/contactUs'
+    },
+    {
+      text: 'Rate Us',
+      icon: StarPurple500,
+      path: '/rateUs'
+    }
+  ],
+  menuThree: [
+    {
+      text: 'Terms & Conditions',
+      icon: Feed,
+      path: '/termsCondition'
+    }
+  ]
+}
--- src/utils/colors.tsx
+++ src/utils/colors.tsx
@@ -1,2 +1,3 @@
 export const APP_BG_LIGHT = "#88d8bc"
 export const APP_BG_DARK = "#135D66"
+export const APP_BG_GREY = "#aaaaaa80"
